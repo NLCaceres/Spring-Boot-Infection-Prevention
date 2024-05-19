1f992f4fcc05a09eef108ebecaf89a71e802945b
NLCaceres <NLCaceres21@Gmail.com>
2024-05-18 23:57:45 -0700

    Add JsonViews class to control which fields API Controllers can return in JSON responses

---
+++ src/main/java/edu/usc/nlcaceres/infectionprotection_backend/models/JsonViews.java
@@ -0,0 +1,6 @@
+package edu.usc.nlcaceres.infectionprotection_backend.models;
+
+//? Interfaces can EXTEND multiple other interfaces. Classes can IMPLEMENT multiple interfaces and EXTEND only one class
+public class JsonViews {
+    public interface Public {}
+}
