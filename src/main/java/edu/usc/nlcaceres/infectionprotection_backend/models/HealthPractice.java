c819c25aa69787164fb94d403a6b7d76add8ac66
NLCaceres <NLCaceres21@Gmail.com>
2024-05-27 16:41:59 -0700

    Fix cyclic references between Precaution and HealthPractice in a more Spring+Jackson compatible way - Replace Lombok @Data annotation with @AllArgs @NoArgs @Getter and @Setter   - Use static factory methods to imitate constructor overloads - Manually implement `equals()`, `hashCode()`, and `toString()` to prevent stack overflows - Use @NonNull to enforce null checks by default

--- src/main/java/edu/usc/nlcaceres/infectionprotection_backend/models/HealthPractice.java
+++ src/main/java/edu/usc/nlcaceres/infectionprotection_backend/models/HealthPractice.java
@@ -1,21 +1,82 @@
 package edu.usc.nlcaceres.infectionprotection_backend.models;
 
-import com.fasterxml.jackson.annotation.JsonView;
-import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
+import org.springframework.lang.Nullable;
+import com.fasterxml.jackson.annotation.JsonView;
+import lombok.NonNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-// Could also add a @JsonInclude(Include.NON_NULL) to ensure null values never get sent to MongoDb
-@Data // Since each HealthPractice has a ref to its parent Precaution, and vice versa, the generated toString can cause Stack Overflows
-@RequiredArgsConstructor // if not cut off somewhere down the cyclic/recursive references
-@Document(collection = "healthpractices") // JsonIdentityInfo is one way to limit recursion BUT isn't perfect in One-to-Many relationships
-//@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+//? To limit recursion, there's @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+//? BUT isn't great with 1-Many Relationships, so there's also @JsonInclude(Include.NON_NULL) to prevent null values from being sent to MongoDB
+@Document(collection = "healthpractices") @AllArgsConstructor @NoArgsConstructor @Getter @Setter
 public class HealthPractice {
-    @JsonView({ JsonViews.Public.class, Precaution.PublicJsonView.class }) @Id @NonNull private String id;
+    //? @AllArgsConstructor basically creates the following (just without the this() delegation and explicit use of setter)
+    // public HealthPractice(String id, String name, @Nullable Precaution precaution) {
+    //     //? Delegate to a Lombok RequiredArgsConstructor-style constructor, enforcing @NonNull fields/props, throwing a NullPointerException if needed
+    //     this(id, name);
+    //     //* THEN just set the precaution of the newly made HealthPractice */
+    //     this.setPrecaution(precaution);
+    // }
+    public static HealthPractice of(String id, String name) {
+        return new HealthPractice(id, name, null);
+    }
+
+    @JsonView({ JsonViews.Public.class, Precaution.PublicJsonView.class }) @NonNull @Id private String id;
+
     @JsonView({ JsonViews.Public.class, Precaution.PublicJsonView.class }) @NonNull private String name;
-    // HealthPractice accessed via a parent Precaution should set this Precaution ref to null to avoid cyclic references
-    @ToString.Exclude // Excluding this property from Lombok's generated toString should prevent overflows
+
+    //? Lazy without Jackson @JsonView isn't enough to avoid the HealthPractice-Precaution cyclic ref
     @DocumentReference(lazy = true) @JsonView(JsonViews.Public.class)
-    private Precaution precaution; // Lazy isn't enough if Jackson or toString recursively loads this ref in
+    @Nullable private Precaution precaution;
+
+    @Override
+    public boolean equals(@Nullable Object obj) { //? @Nullable required to match original param type signature
+        if (this == obj) { return true; } //? Memory address check
+        if (obj == null) { return false; }
+        if (getClass() != obj.getClass()) { return false; }
+
+        HealthPractice other = (HealthPractice) obj;
+
+        if (!id.equals(other.id) || !name.equals(other.name)) { //? `x.equals(null)` ALWAYS returns false
+            return false;
+        }
+
+        if (precaution != null && other.precaution != null) { //? Avoiding an endless loop set off by precaution.equals(other.precaution)
+            return precaution.getId().equals(other.precaution.getId()) && precaution.getName().equals(other.precaution.getName());
+        }
+        else if (precaution == null && other.precaution == null) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    //? Referencing precaution in toString and hashcode causes cyclic refs which lead to a stack overflow
+    @Override
+    public String toString() {
+        String healthPracticeInfo = "HealthPractice(id=" + id + ", name=" + name;
+        return precaution != null ? healthPracticeInfo + ", precautionType=" + precaution.getName() + ")" : healthPracticeInfo + ")";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id.hashCode();
+        result = prime * result + name.hashCode();
+        if (precaution == null) {
+            result = prime * result;
+        }
+        else {
+            result = prime * result + precaution.getId().hashCode();
+            result = prime * result + precaution.getName().hashCode();
+        }
+        return result;
+    }
 }
--- src/main/java/edu/usc/nlcaceres/infectionprotection_backend/models/Precaution.java
+++ src/main/java/edu/usc/nlcaceres/infectionprotection_backend/models/Precaution.java
@@ -1,27 +1,102 @@
 package edu.usc.nlcaceres.infectionprotection_backend.models;
 
-import com.fasterxml.jackson.annotation.JsonView;
-import lombok.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Comparator;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.ReadOnlyProperty;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
-import java.util.List;
+import org.springframework.lang.Nullable;
+import com.fasterxml.jackson.annotation.JsonView;
+import lombok.NonNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data @RequiredArgsConstructor
-@Document(collection = "precautions")
+//? Spring-Boot's Data lib prefers making an @AllArgsConstructor and letting factory methods like Precaution of() handle "overloaded" params
+//? BUT Jackson NEEDS a @NoArgsConstructor for parsing JSON to Objs and vice-versa (though it could more slowly use a constructor marked with @JsonCreator)
+//? SO Spring will ALSO use the @NoArgsConstructor to build instances of the model like it's the empty Java class default constructor
+//? IF I wanted Spring to use the factory methods instead, THEN I could mark it with @PersistanceCreator BUT it's simpler to let Spring decide
+@Document(collection = "precautions") @AllArgsConstructor @NoArgsConstructor @Getter @Setter
 public class Precaution {
-    @JsonView({ JsonViews.Public.class, PublicJsonView.class }) @Id @NonNull private String id;
+
+    public static Precaution of(String id, String name) {
+        return new Precaution(id, name, new ArrayList<>());
+    }
+
+    @JsonView({ JsonViews.Public.class, PublicJsonView.class }) @NonNull @Id private String id;
+
     @JsonView({ JsonViews.Public.class, PublicJsonView.class }) @NonNull private String name;
-    // Usually access HealthPractices via a parent Precaution so the practicesList may be empty but never null
-    @ToString.Exclude @JsonView(PublicJsonView.class)
-    @ReadOnlyProperty @DocumentReference(lookup="{ 'precaution':?#{#self._id} }")
-    private List<HealthPractice> healthPractices;
+
     //? ReadOnlyProperty's will NOT be saved to its MongoDB document, instead we can use this document's ID to query for
     //? HealthPractices with matching IDs in a 'precaution' BSON property via @DocumentReference(lookup)
-    //? Which is a bit better than using @JsonManagedReference (with @JsonBackReference for HealthPractice's Precaution prop)
-    //? Unfortunately ManagedRef & BackRef cause the List to print in the Precaution JSON, but NOT the Precaution in the HealthPractice JSON
+    @JsonView(PublicJsonView.class) @ReadOnlyProperty @DocumentReference(lookup="{ 'precaution':?#{#self._id} }")
+    private List<HealthPractice> healthPractices;
+    //? @DocumentReference(lookup) is better than using Jackson's @JsonManagedReference here with @JsonBackReference in HealthPractice
 
+    @Override
+    public String toString() {
+        String precautionInfo = "Precaution(id=" + id + ", name=" + name + ", healthPractices=[";
+        String healthPracticeInfo = healthPractices.stream().reduce("", (finalStr, healthPractice) -> {
+            return finalStr + healthPractice.getName() + ", ";
+        }, String::concat);
+        String finalHealthPracticeInfo =
+            healthPracticeInfo.endsWith(", ") ? healthPracticeInfo.substring(0, healthPracticeInfo.length() - 2) : "";
+        return precautionInfo + finalHealthPracticeInfo + "])";
+    }
+    
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        if (this == obj) { return true; }
+        if (obj == null) { return false; }
+        if (getClass() != obj.getClass()) { return false; }
+
+        Precaution other = (Precaution) obj;
+
+        if (!id.equals(other.id) || !name.equals(other.name)) {
+            return false;
+        }
+
+        if (healthPractices == null && other.healthPractices == null) {
+            return true;
+        }
+        //? Since prev condition checks if BOTH healthPractice fields are null, now ensure BOTH lists are NOT null AND the same size
+        if (!(healthPractices != null && other.healthPractices != null) || healthPractices.size() != other.healthPractices.size()) {
+            return false; //* Made it here if one or the other healthPractice list is null OR they're different size
+        }
+        //? To avoid setting off a stack overflow calling healthPractices.equals(other.healthPractices)
+        //? Compare the lists one element at a time, BUT first sort them to avoid the comparison failing due to order
+        List<HealthPractice> sortedHealthPractices = new ArrayList<>(healthPractices);
+        List<HealthPractice> sortedOtherHealthPractices = new ArrayList<>(other.healthPractices);
+        sortedHealthPractices.sort(Comparator.comparing(HealthPractice::getId).thenComparing(Comparator.comparing(HealthPractice::getName)));
+        sortedOtherHealthPractices.sort(Comparator.comparing(HealthPractice::getId).thenComparing(Comparator.comparing(HealthPractice::getName)));
+        for (int i = 0; i < sortedHealthPractices.size(); i++) {
+            HealthPractice healthPractice = sortedHealthPractices.get(i);
+            HealthPractice otherHealthPractice = sortedOtherHealthPractices.get(i);
+            if (otherHealthPractice == null ||
+                !healthPractice.getId().equals(otherHealthPractice.getId()) || !healthPractice.getName().equals(otherHealthPractice.getName())) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id.hashCode();
+        result = prime * result + name.hashCode();
+        //? Can't use .forEach() since using vars outside the lambda scope must be final
+        for (HealthPractice healthPractice : healthPractices) {
+            result = prime * result + healthPractice.getId().hashCode();
+            result = prime * result + healthPractice.getName().hashCode();
+        }
+        return result;
+    }
     //* The following interface lets Precaution set its own JSON shape, which will help the Precaution REST API controller create a better response
     //* that differs from what other REST API controllers are able to see of the Precaution type via the default/common JsonViews.Public annotation
     public interface PublicJsonView {}
