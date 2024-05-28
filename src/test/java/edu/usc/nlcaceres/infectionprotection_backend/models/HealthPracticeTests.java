8514967cd2b3a76dfc1c4938091fd4c9151ab4e3
NLCaceres <NLCaceres21@Gmail.com>
2024-05-27 17:07:37 -0700

    Test the manual implementations of `equals()` and `toString()`

--- src/test/java/edu/usc/nlcaceres/infectionprotection_backend/models/HealthPracticeTests.java
+++ src/test/java/edu/usc/nlcaceres/infectionprotection_backend/models/HealthPracticeTests.java
@@ -4,12 +4,39 @@ import edu.usc.nlcaceres.infectionprotection_backend.ModelFactory;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
+//* VERY important to check HealthPractice and Precaution equals() and toString() because of their cyclic references */
+//* If used Lombok to generate those two funcs, then those cyclic refs would cause overflows */
 public class HealthPracticeTests {
     @Test
-    public void removeBackReferencesInParentPrecaution() {
+    public void testEquals() {
         HealthPractice healthPractice = ModelFactory.getHealthPractice(null);
-        assertThat(healthPractice.getName()).isEqualTo("Hand Hygiene"); // Sanity check that data is init properly
-        assertThat(healthPractice.getPrecaution()).isNotNull();
-        assertThat(healthPractice.getPrecaution().getHealthPractices()).isNotEmpty();
+        HealthPractice other = new HealthPractice("abc", "Foo", Precaution.of("def", "Standard"));
+        //* WHEN the other healthPractice only matches in ID, THEN equals returns false */
+        assertThat(healthPractice.equals(other)).isFalse();
+        
+        HealthPractice another = new HealthPractice("abc", "Hand Hygiene", Precaution.of("def", "Standard"));
+        //* WHEN the other healthPractice only matches in ID and name but not precaution, THEN equals returns false */
+        assertThat(healthPractice.equals(another)).isFalse();
+        
+        HealthPractice fullMatch = new HealthPractice("abc", "Hand Hygiene", Precaution.of("cba", "Standard"));
+        //* WHEN the other healthPractice fully matches (including the Precaution id and name), THEN equals returns true */
+        assertThat(healthPractice.equals(fullMatch)).isTrue();
+    }
+    @Test
+    public void testToString() {
+        HealthPractice healthPractice = ModelFactory.getHealthPractice(null);
+        //* Sanity check that data is init properly */
+        assertThat(healthPractice.getId()).isEqualTo("abc");
+        assertThat(healthPractice.getName()).isEqualTo("Hand Hygiene");
+        //* Since the precaution prop is nullable, it's best for the linter to grab it via the getter and initialize a new var */
+        Precaution precaution = healthPractice.getPrecaution();
+        assertThat(precaution).isNotNull(); //? AND because the linter doesn't recognize this line,
+        if (precaution != null) { //? An actual null check is still required
+            assertThat(precaution.getName()).isEqualTo("Standard");
+            assertThat(precaution.getHealthPractices()).isNotEmpty(); //* The backRef to this healthPractice is NOT NULL */
+        }
+
+        //* toString() should return the values for ID, name and the parent precaution's name (as precautionType) */
+        assertThat(healthPractice.toString()).isEqualTo("HealthPractice(id=abc, name=Hand Hygiene, precautionType=Standard)");
     }
 }
--- src/test/java/edu/usc/nlcaceres/infectionprotection_backend/models/PrecautionTests.java
+++ src/test/java/edu/usc/nlcaceres/infectionprotection_backend/models/PrecautionTests.java
@@ -4,12 +4,43 @@ import edu.usc.nlcaceres.infectionprotection_backend.ModelFactory;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
+//* VERY important to check HealthPractice and Precaution equals() and toString() because of their cyclic references */
+//* If used Lombok to generate those two funcs, then those cyclic refs would cause overflows */
 public class PrecautionTests {
     @Test
-    public void removeBackReferenceInChildHealthPractices() {
+    public void testEquals() {
         Precaution precaution = ModelFactory.getPrecaution(null);
-        assertThat(precaution.getName()).isEqualTo("Isolation"); // Sanity Checks that data is init as expected
-        assertThat(precaution.getHealthPractices()).isNotEmpty();
+        Precaution other = Precaution.of("abc", "Foo");
+        //* WHEN only the IDs match, THEN equals() returns false */
+        assertThat(precaution.equals(other)).isFalse();
+
+        Precaution another = Precaution.of("abc", "Isolation");
+        //* WHEN the IDs and names match but not the list of health practices (size included), THEN equals() returns false */
+        assertThat(precaution.equals(another)).isFalse();
+
+        Precaution fullMatch = Precaution.of("abc", "Isolation");
+        fullMatch.getHealthPractices().add(HealthPractice.of("abc", "PPE"));
+        fullMatch.getHealthPractices().add(HealthPractice.of("abc", "Hand Hygiene"));
+        //* WHEN all fields match including each HealthPractice's id and name (after sorting), THEN equals() returns true */
+        assertThat(precaution.equals(fullMatch)).isTrue();
+    }
+    @Test
+    public void testToString() {
+        Precaution precaution = ModelFactory.getPrecaution(null);
+        //* Sanity checks that the data is init as expected */
+        assertThat(precaution.getId()).isEqualTo("abc");
+        assertThat(precaution.getName()).isEqualTo("Isolation");
+        //* HealthPractices shouldn't be null and at least an empty list. ALSO, the backRef to the precaution should also be non-null */
+        assertThat(precaution.getHealthPractices()).hasSize(2);
         precaution.getHealthPractices().forEach(healthPractice -> assertThat(healthPractice.getPrecaution()).isNotNull());
+        assertThat(precaution.getHealthPractices().get(0).getName()).isEqualTo("Hand Hygiene");
+        assertThat(precaution.getHealthPractices().get(1).getName()).isEqualTo("PPE");
+
+        //* toString() should return the values for id, name and every health practice's name in a list */
+        assertThat(precaution.toString()).isEqualTo("Precaution(id=abc, name=Isolation, healthPractices=[Hand Hygiene, PPE])");
+
+        Precaution other = Precaution.of("abc", "Isolation");
+        //* WHEN there are no health practices in the list, THEN the health practices list is empty in toString() */
+        assertThat(other.toString()).isEqualTo("Precaution(id=abc, name=Isolation, healthPractices=[])");
     }
 }
