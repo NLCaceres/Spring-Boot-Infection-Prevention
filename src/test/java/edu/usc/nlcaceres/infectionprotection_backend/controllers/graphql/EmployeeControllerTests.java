304558ae131e7fa39391c321babba1cb61483d52
NLCaceres <NLCaceres21@Gmail.com>
2024-06-03 02:02:21 -0700

    Fix EmployeeGraphQLController usage test when Profession field is not requested - Originally Lombok's @RequiredArgsConstructor was used to parse the GraphQL response into an Employee instance causing the tester to throw a DecodingException if no Profession requested - NOW since the @NoArgsConstructor is the default decoder, the tester no longer throws if no Profession is requested, allowing a null Profession field to be found and tested

--- src/test/java/edu/usc/nlcaceres/infectionprotection_backend/controllers/graphql/EmployeeControllerTests.java
+++ src/test/java/edu/usc/nlcaceres/infectionprotection_backend/controllers/graphql/EmployeeControllerTests.java
@@ -10,11 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.graphql.GraphQlTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.core.codec.DecodingException;
 import org.springframework.graphql.test.tester.GraphQlTester;
 import org.springframework.test.context.aot.DisabledInAotMode;
-
 import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 @DisabledInAotMode
@@ -33,15 +32,19 @@ public class EmployeeControllerTests {
         List<Employee> mockEmployeeList = ModelFactory.getEmployeeList();
         when(employeeService.getAll()).thenReturn(mockEmployeeList);
 
-        // Decoding error thrown here due to invalid JSON returned BUT GraphQL's "!" non-null operator does NOT guarantee that
-        // all fields will be returned or else throw. Instead, it simply lets clients know that those fields will always be non-null if requested
-        try { // So the real issue with the following query is the Employee Lombok @NonNull class declaration, NOT the Controller
-            this.tester.document("{ employees { id firstName surname } }").execute().path("employees").entityList(Employee.class).get();
-        } catch (DecodingException error) {
-            System.out.println("Incomplete Employee JSON sent causing DecodingException. Spring unable to instantiate Employee object");
-        }
-        this.tester.document("{ employees { id firstName surname profession { id observedOccupation serviceDiscipline } } }").execute()
-                .path("employees").entityList(Employee.class).hasSize(2);
+        //? BEFORE Jackson defaulted to the @RequiredArgsConstructor WHICH caused a DecodingException if it didn't find a Profession key-value
+        //* NOW Jackson uses the @NoArgsConstructor, allowing GraphQL to NOT return a Profession IF NOT requested */
+        //? SO the GraphQL "!" non-null operator JUST indicates to clients that the Profession won't be null IF requested
+        List<Employee> employees = this.tester.document("{ employees { id firstName surname } }").execute().path("employees")
+            .entityList(Employee.class).get();
+        assertThat(employees).hasSize(2);
+        employees.forEach(employee -> assertThat(employee.getProfession()).isNull());
+
+        //* Requesting 'profession' now fills the Profession field of the Employees as expected, of course */
+        List<Employee> fullEmployees = this.tester.document("{ employees { id firstName surname profession { id observedOccupation serviceDiscipline } } }")
+            .execute().path("employees").entityList(Employee.class).get();
+        assertThat(fullEmployees).hasSize(2);
+        fullEmployees.forEach(employee -> assertThat(employee.getProfession()).isNotNull());
     }
 
     @Test
@@ -49,8 +52,8 @@ public class EmployeeControllerTests {
         Employee mockEmployee = ModelFactory.getEmployee(null, null, null);
         when(employeeService.getById("abc")).thenReturn(mockEmployee);
 
-        // An alternative and, technically, more accurate view into the response GraphQL sends is the following example
-        // BUT it isn't the best solution for Lists since they can be fairly long and tough to assert upon in JSON form
+        //? An alternative and, technically, more accurate view into the response GraphQL sends is the following example
+        //? BUT it isn't the best solution for Lists since they can be fairly long and tough to assert upon in JSON form
         this.tester.document("{ employeeById(id: \"abc\") { firstName surname } }").execute().path("employeeById")
                 .matchesJson("{ \"firstName\": \"Melody\", \"surname\": \"Rios\" } ");
     }
