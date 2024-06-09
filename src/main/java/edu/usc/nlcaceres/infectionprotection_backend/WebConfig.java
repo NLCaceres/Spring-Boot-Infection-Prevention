60ce1c2488db03bad7d02b1d56df0b9194d22aef
NLCaceres <NLCaceres21@Gmail.com>
2024-06-08 17:25:46 -0700

    Fix Non-Null typing issue with WebMvc Config

--- src/main/java/edu/usc/nlcaceres/infectionprotection_backend/WebConfig.java
+++ src/main/java/edu/usc/nlcaceres/infectionprotection_backend/WebConfig.java
@@ -1,6 +1,7 @@
 package edu.usc.nlcaceres.infectionprotection_backend;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.util.pattern.PathPatternParser;
@@ -8,7 +9,7 @@ import org.springframework.web.util.pattern.PathPatternParser;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
-    public void configurePathMatch(PathMatchConfigurer configurer) {
+    public void configurePathMatch(@NonNull PathMatchConfigurer configurer) {
         PathPatternParser patternParser = new PathPatternParser();
         patternParser.setCaseSensitive(false);
         configurer.setPatternParser(patternParser);
