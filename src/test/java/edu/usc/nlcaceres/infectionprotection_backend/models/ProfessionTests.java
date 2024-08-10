1724cc28fdab3b8a02f840cccbbbf61d379d1d17
NLCaceres <NLCaceres21@Gmail.com>
2024-08-10 14:25:48 -0700

    Test static factory methods init Professions with specific state + format - Ensure that the underlying constructors don't throw a `NullPointerException` - Format comments for `Better-Comments` to highlight

--- src/test/java/edu/usc/nlcaceres/infectionprotection_backend/models/ProfessionTests.java
+++ src/test/java/edu/usc/nlcaceres/infectionprotection_backend/models/ProfessionTests.java
@@ -6,18 +6,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ProfessionTests {
     @Test
+    public void testStaticFactory() {
+        // - WHEN 3 params are set in the static factory method
+        Profession profession = Profession.of("123", "Foo", "Bar");
+        // - THEN all 3 fields of Profession are correctly set
+        assertThat(profession.getId()).isEqualTo("123");
+        assertThat(profession.getObservedOccupation()).isEqualTo("Foo");
+        assertThat(profession.getServiceDiscipline()).isEqualTo("Bar");
+
+        // - WHEN only 2 params are set in the static factory method
+        Profession professionWithNullID = Profession.of("Barfoo", "Foobar");
+        // - THEN the ID field is set to null, despite @NonNull BUT this lets MongoDB generate & set the ID later
+        assertThat(professionWithNullID.getId()).isNull();
+        // - BUT the other main fields are set correctly
+        assertThat(professionWithNullID.getObservedOccupation()).isEqualTo("Barfoo");
+        assertThat(professionWithNullID.getServiceDiscipline()).isEqualTo("Foobar");
+    }
+    @Test
     public void testLombokEquals() {
         Profession profession = Profession.of("Foo", "Bar", "Fizz");
-        //* WHEN only the id matches, THEN the Professions are NOT equal */
+        // - WHEN only the id matches, THEN the Professions are NOT equal
         Profession otherProfession = Profession.of("Foo", "Ba", "Fiz");
         assertThat(profession.equals(otherProfession)).isFalse();
 
         Profession anotherProfession = Profession.of("Foo", "Bar", "Fiz");
-        //* WHEN only the id and observed occupation fields' values match, THEN the Professions are NOT equal */
+        // - WHEN only the id and observed occupation fields' values match, THEN the Professions are NOT equal
         assertThat(profession.equals(anotherProfession)).isFalse();
 
         Profession finalProfession = Profession.of("Foo", "Bar", "Fizz");
-        //* WHEN all field values match, THEN the Professions are equal */
+        // - WHEN all field values match, THEN the Professions are equal
         assertThat(profession.equals(finalProfession)).isTrue();
     }
     @Test
