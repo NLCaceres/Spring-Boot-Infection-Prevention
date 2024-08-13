48cebd620c09327230c270076d4c5f7eb8e61a10
NLCaceres <NLCaceres21@Gmail.com>
2024-08-12 23:55:57 -0700

    Add Distinct findBy Query to `Profession` MongoRepository - Selects a single `Profession` based on a distinct/unique set of `observedOccupation` and `serviceDiscipline` fields

--- src/main/java/edu/usc/nlcaceres/infectionprotection_backend/repositories/ProfessionRepository.java
+++ src/main/java/edu/usc/nlcaceres/infectionprotection_backend/repositories/ProfessionRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProfessionRepository extends MongoRepository<Profession, String> {
+  Profession findFirstProfessionDistinctByObservedOccupationAndServiceDiscipline(String observedOccupation, String serviceDiscipline);
 }
