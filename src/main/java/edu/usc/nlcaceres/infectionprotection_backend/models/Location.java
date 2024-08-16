872c4433bc3e11543f464988196e2996e33264c1
NLCaceres <NLCaceres21@Gmail.com>
2024-08-15 23:59:12 -0700

    Add Location static factory to allow null IDs + format comments

--- src/main/java/edu/usc/nlcaceres/infectionprotection_backend/models/Location.java
+++ src/main/java/edu/usc/nlcaceres/infectionprotection_backend/models/Location.java
@@ -18,12 +18,19 @@ public class Location {
     public static Location of(String id, String facilityName, String unitNum, String roomNum) {
         return new Location(id, facilityName, unitNum, roomNum);
     }
+    public static Location of(String facilityName, String unitNum, String roomNum) {
+        Location location = new Location();
+        location.setFacilityName(facilityName);
+        location.setUnitNum(unitNum);
+        location.setRoomNum(roomNum);
+        return location;
+    }
 
     @NonNull @Id private String id;
 
-    //* Why change field names to snake_case? It is common convention in SQL based databases even if MongoDB seems to prefer
-    //* the camelCase style due to its Javascript roots. If I switch to SQL style snake_case though, migration would be simpler
-    //* Overall MongoDB doesn't really seem to care BUT SQL databases DO CARE so easiest to fall in line with the SQL convention
+    // - Why change field names to snake_case? It is common convention in SQL based databases even if MongoDB seems to prefer
+    // - the camelCase style due to its Javascript roots. If I switch to SQL style snake_case though, migration would be simpler
+    // - Overall MongoDB doesn't really seem to care BUT SQL databases DO CARE so easiest to fall in line with the SQL convention
     @NonNull private String facilityName; // TODO: Use Mongock to create a MongoDB update script and convert the camelCase field names to snake_case
 
     @NonNull private String unitNum;
--- src/test/java/edu/usc/nlcaceres/infectionprotection_backend/models/LocationTests.java
+++ src/test/java/edu/usc/nlcaceres/infectionprotection_backend/models/LocationTests.java
@@ -6,17 +6,36 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class LocationTests {
     @Test
+    public void testStaticFactory() {
+        // - WHEN 4 params are set in the static factory method
+        Location location = Location.of("123", "Foo", "1", "12");
+        // - THEN all 4 fields of Location are correctly set
+        assertThat(location.getId()).isEqualTo("123");
+        assertThat(location.getFacilityName()).isEqualTo("Foo");
+        assertThat(location.getUnitNum()).isEqualTo("1");
+        assertThat(location.getRoomNum()).isEqualTo("12");
+
+        // - WHEN only 3 params are set in the static factory method
+        Location locationWithNullID = Location.of("Bar", "2", "213");
+        // - THEN the ID field is set to null, despite @NonNull BUT this lets MongoDB generate & set the ID later
+        assertThat(locationWithNullID.getId()).isNull();
+        // - BUT the other main fields are set correctly
+        assertThat(locationWithNullID.getFacilityName()).isEqualTo("Bar");
+        assertThat(locationWithNullID.getUnitNum()).isEqualTo("2");
+        assertThat(locationWithNullID.getRoomNum()).isEqualTo("213");
+    }
+    @Test
     public void testLombokEquals() {
         Location location = Location.of("Abc", "Facility", "Unit", "Room");
-        //* WHEN only the id matches, THEN the locations are NOT equal */
+        // - WHEN only the id matches, THEN the locations are NOT equal
         Location otherLocation = Location.of("Abc", "facility", "unit", "room");
         assertThat(location.equals(otherLocation)).isFalse();
 
-        //* WHEN only the id and facility names matches, THEN the locations are NOT equal */
+        // - WHEN only the id and facility names matches, THEN the locations are NOT equal
         Location anotherLocation = Location.of("Abc", "Facility", "unit", "room");
         assertThat(location.equals(anotherLocation)).isFalse();
 
-        //* WHEN all field values matches, THEN the locations are equal */
+        // - WHEN all field values matches, THEN the locations are equal
         Location finalLocation = Location.of("Abc", "Facility", "Unit", "Room");
         assertThat(location.equals(finalLocation)).isTrue();
     }
