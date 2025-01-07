d201f575054bccd04233b0c058558192023f5002
NLCaceres <NLCaceres21@Gmail.com>
2025-01-07 13:11:48 -0800

    Fix Jar ChangeUnit issue due to out of order operations
    - Before: Renaming operations from "camelCase" fields to "snake_case" could
    seemingly fail the query or renaming because one of their other fields had
    already been renamed
    - Now: Since every operation is done in order and in bulk, each document
    is properly renamed all at once before moving on

--- src/main/java/edu/usc/nlcaceres/infectionprotection_backend/migrations/RenameLocationsFieldsChangeUnit.java
+++ src/main/java/edu/usc/nlcaceres/infectionprotection_backend/migrations/RenameLocationsFieldsChangeUnit.java
@@ -1,6 +1,7 @@
 package edu.usc.nlcaceres.infectionprotection_backend.migrations;
 
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.BulkOperations.BulkMode;
 import io.mongock.api.annotations.ChangeUnit;
 import io.mongock.api.annotations.Execution;
 import io.mongock.api.annotations.RollbackExecution;
@@ -8,33 +9,31 @@ import org.springframework.data.mongodb.core.query.Update;
 import edu.usc.nlcaceres.infectionprotection_backend.models.Location;
 import org.springframework.data.mongodb.core.query.Criteria;
 import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
 
 @ChangeUnit(id="rename-locations-fields", order="4")
 public class RenameLocationsFieldsChangeUnit {
   @Execution
   public void execution(MongoTemplate mongoTemplate) {
-      mongoTemplate.update(Location.class)
-        .matching( // ?: Alternatively, a BasicQuery could take a String version of any query
-          new Criteria().orOperator(
-            where("facilityName").exists(true), where("unitNum").exists(true), where("roomNum").exists(true)
-          )
-        )
-        .apply(
-          new Update().rename("facilityName", "facility_name")
-            .rename("unitNum", "unit_num").rename("roomNum", "room_num")
-        ).all();
+    // ?: Bulk Mode (vs the typical batch insert) ensures documents are atomically processed
+    mongoTemplate.bulkOps(BulkMode.ORDERED, Location.class)
+      .updateMulti(
+        query(new Criteria().orOperator( // ?: A BasicQuery takes a String version of this query
+          where("facilityName").exists(true), where("unitNum").exists(true), where("roomNum").exists(true)
+        )),
+        new Update().rename("facilityName", "facility_name")
+          .rename("unitNum", "unit_num").rename("roomNum", "room_num")
+      );
   }
   @RollbackExecution
   public void rollbackExecution(MongoTemplate mongoTemplate) {
-      mongoTemplate.update(Location.class)
-        .matching(
-          new Criteria().orOperator(
-            where("facility_name").exists(true), where("unit_num").exists(true), where("room_num").exists(true)
-          )
-        )
-        .apply(
-          new Update().rename("facility_name", "facilityName")
-            .rename("unit_num", "unitNum").rename("room_num", "roomNum")
-        ).all();
+    mongoTemplate.bulkOps(BulkMode.ORDERED, Location.class)
+      .updateMulti(
+        query(new Criteria().orOperator(
+          where("facility_name").exists(true), where("unit_num").exists(true), where("room_num").exists(true)
+        )),
+        new Update().rename("facility_name", "facilityName")
+          .rename("unit_num", "unitNum").rename("room_num", "roomNum")
+      );
   }
 }
