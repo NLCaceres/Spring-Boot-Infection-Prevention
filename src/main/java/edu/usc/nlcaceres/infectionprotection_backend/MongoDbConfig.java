3db721ebc8a0db79243a37cf330183654614655c
NLCaceres <NLCaceres21@Gmail.com>
2025-01-10 23:47:20 -0800

    Drop `Environment` for `@Value` to set Mongo URI via Env vars
    - MUCH more straightforward than `env.getProperty` + getters
    - Also cleaned up usage comments

--- src/main/java/edu/usc/nlcaceres/infectionprotection_backend/MongoDbConfig.java
+++ src/main/java/edu/usc/nlcaceres/infectionprotection_backend/MongoDbConfig.java
@@ -1,79 +1,70 @@
 package edu.usc.nlcaceres.infectionprotection_backend;
 
 import com.mongodb.ConnectionString;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.core.MongoClientFactoryBean;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
-@Configuration
+// ?: AbstractMongoClientConfiguration is also an option, overriding its MongoClient
+// ?: and DbName methods BUT this MongoDbConfig seems more Spring-like,
+@Configuration // ?: despite the sorta unclear docs
 public class MongoDbConfig {
-    // ? Alternatively could extend AbstractMongoClientConfiguration and override its mongoClient + dbName methods
-    // ? BUT I wanted to go with something that seems more in line with what Spring's Ref Docs would prefer (even if the docs are kinda unclear)
-    @Autowired
-    Environment env;
+    @Value("${SPRING_ENV:}")
+    private String springEnv;
 
-    private String getSpringEnv() {
-        return env.getProperty("SPRING_ENV", "");
-    }
+    @Value("${MONGO_USER:}")
+    private String mongoUser;
+
+    @Value("${MONGO_PASSWORD:}")
+    private String mongoPassword;
+
+    @Value("${MONGO_HOST:}")
+    private String mongoHost;
+
+    @Value("${MONGO_APP_NAME:}")
+    private String mongoAppName;
+
+    @Value("${MONGO_DB:dev}")
+    private String databaseName;
+
     private boolean isDevMode() {
-        return getSpringEnv().equals("dev");
+        return springEnv.equals("dev");
     }
     private boolean isTesting() {
-        return getSpringEnv().equals("test");
-    }
-    private String getDatabaseProtocol() {
-        return isDevMode() ? "mongodb" : "mongodb+srv";
-    }
-    private String getDatabaseName() {
-        return env.getProperty("MONGO_DB", "dev");
-    }
-    private String getMongoUser() {
-        return isDevMode() ? "root" : env.getProperty("MONGO_USER", "");
-    }
-    private String getMongoPassword() {
-        return isDevMode() ? "example" : env.getProperty("MONGO_PASSWORD", "");
-    }
-    private String getMongoHost() {
-        return isTesting() ? "localhost:27017" :
-            isDevMode() ? "mongodb:27017" : env.getProperty("MONGO_HOST", "");
+        return springEnv.equals("test");
     }
     private String mongoURI() {
-        // * Using StringBuilder to make the following String:
-        // * "mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}/?retryWrites=true&w=majority&appName=Cluster0"
+        // - Using StringBuilder to form a Mongo URL similar to:
+        // - "mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}"
         StringBuilder sb = new StringBuilder(124);
-        sb.append(getDatabaseProtocol());
+        sb.append(isDevMode() ? "mongodb" : "mongodb+srv");
         sb.append("://");
-        sb.append(getMongoUser());
+        sb.append(isDevMode() ? "root" : mongoUser);
         sb.append(":");
-        sb.append(getMongoPassword());
+        sb.append(isDevMode() ? "example" : mongoPassword);
         sb.append("@");
-        sb.append(getMongoHost());
+        sb.append(isTesting() ? "localhost:27017" : isDevMode() ? "mongodb:27017" : mongoHost);
         sb.append("/");
-        // ? Normally would set the DB name below BUT slightly simpler to let the MongoTemplate bean do it below
-        // sb.append(getDatabaseName());
-        // ? Since no need to set the DB Name, just set the queryParam-based options
+        // ?: Since no need to set the DB Name, just set the queryParam-based options
         sb.append("?retryWrites=true&w=majority&appName=");
-        // ? The "appName" option is fairly new and helps identify the client, particularly, in logs kept by the MongoDB Host
-        sb.append(env.getProperty("MONGO_APP_NAME", ""));
+        // ?: The "appName" option is fairly new and helps identify the client
+        sb.append(mongoAppName); // ?: particularly in logs kept by Mongo itself
         return sb.toString();
     }
 
-    // ? Better to use Spring's MongoClientFactoryBean over the actual MongoClient type since the FactoryBean will
-    // ? display more Spring-oriented error messaging if something goes wrong + more configuration options!
-    @Bean
+    // ?: Better to use Spring's MongoClientFactoryBean over the actual MongoClient type
+    @Bean // ?: since Spring's FactoryBean displays better error messages + is more configurable
     public MongoClientFactoryBean mongoClient() {
         MongoClientFactoryBean mongo = new MongoClientFactoryBean();
         mongo.setConnectionString(new ConnectionString(mongoURI()));
         return mongo;
     }
 
-    // ? MongoTemplates are responsible for actual operations on the MongoDB instance, so have to make my own Bean to ensure
-    // ? my customized connection created above is actually used by this app's MongoRepos when the App loads all the Beans
-    @Bean
-    public MongoTemplate mongoTemplate() throws Exception {
-        return new MongoTemplate(mongoClient().getObject(), getDatabaseName());
+    // ?: MongoTemplates perform MongoDB operations so must make a custom Bean for the
+    @Bean // ?: app MongoRepositories use to connect and modify the database
+    public MongoTemplate mongoTemplate() throws Exception { // ?: Set DB name here, NOT in mongoURI()
+        return new MongoTemplate(mongoClient().getObject(), databaseName);
     }
 }
