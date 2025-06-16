d500526f3e5f053b206c2bebdb0e47a6b1a2d511
NLCaceres <NLCaceres21@Gmail.com>
2025-06-16 13:59:14 -0700

    Re-arrange methods to place getter & predicate methods first
    - Since the getters and predicates are used by `mongoURI` to construct the
    Mongo database URL, it makes sense to place them first, especially since they're
    fairly short and simple to understand.
    - Also re-formats file for 2 spaces instead of 4.

--- src/main/java/edu/usc/nlcaceres/infectionprotection_backend/MongoDbConfig.java
+++ src/main/java/edu/usc/nlcaceres/infectionprotection_backend/MongoDbConfig.java
@@ -11,60 +11,62 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 // ?: and DbName methods BUT this MongoDbConfig seems more Spring-like,
 @Configuration // ?: despite the sorta unclear docs
 public class MongoDbConfig {
-    @Value("${SPRING_ENV:}")
-    private String springEnv;
+  @Value("${SPRING_ENV:}")
+  private String springEnv;
 
-    @Value("${MONGO_USER:}")
-    private String mongoUser;
+  @Value("${MONGO_USER:}")
+  private String mongoUser;
 
-    @Value("${MONGO_PASSWORD:}")
-    private String mongoPassword;
+  @Value("${MONGO_PASSWORD:}")
+  private String mongoPassword;
 
-    @Value("${MONGO_HOST:}")
-    private String mongoHost;
+  @Value("${MONGO_HOST:}")
+  private String mongoHost;
 
-    @Value("${MONGO_APP_NAME:}")
-    private String mongoAppName;
+  @Value("${MONGO_APP_NAME:}")
+  private String mongoAppName;
 
-    @Value("${MONGO_DB:dev}")
-    private String databaseName;
+  @Value("${MONGO_DB:dev}")
+  private String databaseName;
 
-    private boolean isDevMode() {
-        return springEnv.equals("dev");
-    }
-    private boolean isTesting() {
-        return springEnv.equals("test");
-    }
-    private String mongoURI() {
-        // - Using StringBuilder to form a Mongo URL similar to:
-        // - "mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}"
-        StringBuilder sb = new StringBuilder(124);
-        sb.append(isDevMode() ? "mongodb" : "mongodb+srv");
-        sb.append("://");
-        sb.append(isDevMode() ? "root" : mongoUser);
-        sb.append(":");
-        sb.append(isDevMode() ? "example" : mongoPassword);
-        sb.append("@");
-        sb.append(isTesting() ? "localhost:27017" : isDevMode() ? "mongodb:27017" : mongoHost);
-        sb.append("/");
-        // ?: Since no need to set the DB Name, just set the queryParam-based options
-        sb.append("?retryWrites=true&w=majority&appName=");
-        // ?: The "appName" option is fairly new and helps identify the client
-        sb.append(mongoAppName); // ?: particularly in logs kept by Mongo itself
-        return sb.toString();
-    }
+  private boolean isDevMode() {
+    return springEnv.equals("dev");
+  }
 
-    // ?: Better to use Spring's MongoClientFactoryBean over the actual MongoClient type
-    @Bean // ?: since Spring's FactoryBean displays better error messages + is more configurable
-    public MongoClientFactoryBean mongoClient() {
-        MongoClientFactoryBean mongo = new MongoClientFactoryBean();
-        mongo.setConnectionString(new ConnectionString(mongoURI()));
-        return mongo;
-    }
+  private boolean isTesting() {
+    return springEnv.equals("test");
+  }
 
-    // ?: MongoTemplates perform MongoDB operations so must make a custom Bean for the
-    @Bean // ?: app MongoRepositories use to connect and modify the database
-    public MongoTemplate mongoTemplate() throws Exception { // ?: Set DB name here, NOT in mongoURI()
-        return new MongoTemplate(mongoClient().getObject(), databaseName);
-    }
+  private String mongoURI() {
+    // - Using StringBuilder to form a Mongo URL similar to:
+    // - "mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}"
+    StringBuilder sb = new StringBuilder(124);
+    sb.append(isDevMode() ? "mongodb" : "mongodb+srv");
+    sb.append("://");
+    sb.append(isDevMode() ? "root" : mongoUser);
+    sb.append(":");
+    sb.append(isDevMode() ? "example" : mongoPassword);
+    sb.append("@");
+    sb.append(isTesting() ? "localhost:27017" : isDevMode() ? "mongodb:27017" : mongoHost);
+    sb.append("/");
+    // ?: Since no need to set the DB Name, just set the queryParam-based options
+    sb.append("?retryWrites=true&w=majority&appName=");
+    // ?: The "appName" option is fairly new and helps identify the client
+    sb.append(mongoAppName); // ?: particularly in logs kept by Mongo itself
+    return sb.toString();
+  }
+
+  // ?: Better to use Spring's MongoClientFactoryBean over the actual MongoClient type
+  @Bean // ?: since Spring's FactoryBean displays better error messages + is more configurable
+  public MongoClientFactoryBean mongoClient() {
+    MongoClientFactoryBean mongo = new MongoClientFactoryBean();
+    mongo.setConnectionString(new ConnectionString(mongoURI()));
+    return mongo;
+  }
+
+  // ?: MongoTemplates perform MongoDB operations so must make a custom Bean for the
+  @Bean // ?: app MongoRepositories use to connect and modify the database
+  public MongoTemplate mongoTemplate() throws Exception { // ?: Set DB name here, NOT in mongoURI()
+    return new MongoTemplate(mongoClient().getObject(), databaseName);
+  }
 }
